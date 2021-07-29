a5d32b07fad5b4985d6cfab6be5ba4d2d46f62f3
eliphatfs <flandre@scarletx.cn>
2021-07-29 17:34:18 +0800

    Example

---
+++ Assets/Scripts/ExampleExtras/PointSwapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Example physics system interaction script: swapping points
+/// </summary>
+public class PointSwapper : MonoBehaviour
+{
+    public Transform tracked = null;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hitInfo))
+            {
+                if (tracked == null) tracked = hitInfo.transform;
+                else
+                {
+                    var t = hitInfo.transform.position;
+                    hitInfo.transform.position = tracked.position;
+                    tracked.position = t;
+                    tracked = null;
+                }
+            }
+        }
+    }
+}
