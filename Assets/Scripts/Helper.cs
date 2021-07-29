94c98a08c9455c972d184945b23c58b44d29f138
eliphatfs <flandre@scarletx.cn>
2021-07-29 17:01:31 +0800

    Utility scripts

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class Helper
+{
+    public static Vector3 ToVector3(this double[] vector) => new Vector3((float)vector[0], (float)vector[1], (float)vector[2]);
+
+    public static Color ToColor(this double[] vector) =>
+        vector.Length <= 3 ?
+            new Color((float)vector[0], (float)vector[1], (float)vector[2])
+            : new Color((float)vector[0], (float)vector[1], (float)vector[2], (float)vector[3]);
+
+    public static Color ReplaceAlpha(this Color color, float a)
+    {
+        Color n = color; n.a = a; return n;
+    }
+}
---
+++ Assets/Scripts/TransformSnapshot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Helper to snapshot transforms (local coordinates), for quick adjustments in play mode.
+/// Temporary data may be lost after restarting editor.
+/// </summary>
+[DisallowMultipleComponent]
+public class TransformSnapshot : MonoBehaviour
+{
+    public struct Data
+    {
+        public Vector3 position;
+        public Vector3 scale;
+        public Quaternion rotation;
+    }
+
+    public static Data[] storage = new Data[5];
+}
+
+[CustomEditor(typeof(TransformSnapshot))]
+public class TransformSnapshotEditor : Editor
+{
+    public TransformSnapshot edited => target as TransformSnapshot;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        for (var i = 0; i < TransformSnapshot.storage.Length; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button($"Load {i}")) Load(i);
+            if (GUILayout.Button($"Store {i}")) Store(i);
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+
+    public void Store(int i)
+    {
+        TransformSnapshot.storage[i] = new TransformSnapshot.Data
+        {
+            position = edited.transform.localPosition,
+            scale = edited.transform.localScale,
+            rotation = edited.transform.localRotation,
+        };
+    }
+
+    public void Load(int i)
+    {
+        edited.transform.localPosition = TransformSnapshot.storage[i].position;
+        edited.transform.localScale = TransformSnapshot.storage[i].scale;
+        edited.transform.localRotation = TransformSnapshot.storage[i].rotation;
+    }
+}
